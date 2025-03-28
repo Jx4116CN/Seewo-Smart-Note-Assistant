79bb08196d49a3d5313fd11f28de236af86c412a
Jx4116CN <2467895039@qq.com>
2025-03-28 22:23:48 +0800

    code

--- Seewo Note Shot - 配置工具/Form1.Designer.cs
+++ Seewo Note Shot - 配置工具/Form1.Designer.cs
@@ -7,13 +7,7 @@ namespace Seewo_Note_Shot___配置工具
 {
     partial class Form1
     {
-        static Screen screen = Screen.PrimaryScreen;
-        static int screenW = screen.Bounds.Width;
-        static int screenH = screen.Bounds.Height;
-        static int formW = screenW / 5;
-        static int formH = screenH / 2;
-        static int spacing = screenH / 80;
-
+        private int spacing = 11;
         /// <summary>
         /// 必需的设计器变量。
         /// </summary>
@@ -42,58 +36,99 @@ namespace Seewo_Note_Shot___配置工具
 
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(formW, formH);
-            this.MinimumSize = new System.Drawing.Size(formW, formH);
-            this.MaximumSize = new System.Drawing.Size(formW, formH);
-            this.MaximizeBox = false;
-            this.Text = "Seewo Note Shot - 配置工具";
-
-            Init_Console();
-            Init_Finished();
-        }
-
-        private void Init_Console()
-        {
-            Read_Console();
+            this.TabControl1 = new System.Windows.Forms.TabControl();
+            this.TabPage_save = new System.Windows.Forms.TabPage();
+            this.TabPage_debug = new System.Windows.Forms.TabPage();
             this.Label_Console = new System.Windows.Forms.Label();
-            this.Label_Console.Text = "显示控制台";
-            this.Label_Console.Font = new System.Drawing.Font("Microsoft Sans Serif", spacing);
-            this.Label_Console.Width = spacing * 8;
-            this.Label_Console.Height = spacing * 2;
-            this.Label_Console.Left = spacing;
-            this.Label_Console.Top = spacing;
-            this.Controls.Add(this.Label_Console);
-
             this.CheckBox_Console = new System.Windows.Forms.CheckBox();
-            this.CheckBox_Console.Checked = console;
-            this.CheckBox_Console.Width = spacing;
-            this.CheckBox_Console.Height = spacing;
-            this.CheckBox_Console.Left = Label_Console.Right;
-            this.CheckBox_Console.Top = Label_Console.Top + (Label_Console.Height - CheckBox_Console.Height) / 2;
-            this.Controls.Add(this.CheckBox_Console);
-
-        }
-
-        private void Init_Finished()
-        {
             this.Button_Cancel = new System.Windows.Forms.Button();
-            this.Button_Cancel.Size = new System.Drawing.Size(spacing * 5, spacing * 2);
-            this.Button_Cancel.Location = new System.Drawing.Point(formW - spacing * 2 - this.Button_Cancel.Width, formH - spacing * 6);
-            this.Button_Cancel.Name = "取消";
+            this.Button_OK = new System.Windows.Forms.Button();
+            this.TabControl1.SuspendLayout();
+            this.TabPage_debug.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TabControl1
+            // 
+            this.TabControl1.Controls.Add(this.TabPage_save);
+            this.TabControl1.Controls.Add(this.TabPage_debug);
+            this.TabControl1.Font = new System.Drawing.Font("黑体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.TabControl1.Location = new System.Drawing.Point(12, 12);
+            this.TabControl1.Name = "TabControl1";
+            this.TabControl1.SelectedIndex = 0;
+            this.TabControl1.Size = new System.Drawing.Size(320, 548);
+            this.TabControl1.TabIndex = 0;
+            // 
+            // TabPage_save
+            // 
+            this.TabPage_save.Location = new System.Drawing.Point(4, 24);
+            this.TabPage_save.Name = "TabPage_save";
+            this.TabPage_save.Size = new System.Drawing.Size(312, 520);
+            this.TabPage_save.TabIndex = 0;
+            this.TabPage_save.Text = "存储";
+            // 
+            // TabPage_debug
+            // 
+            this.TabPage_debug.Controls.Add(this.Label_Console);
+            this.TabPage_debug.Controls.Add(this.CheckBox_Console);
+            this.TabPage_debug.Location = new System.Drawing.Point(4, 24);
+            this.TabPage_debug.Name = "TabPage_debug";
+            this.TabPage_debug.Size = new System.Drawing.Size(312, 520);
+            this.TabPage_debug.TabIndex = 0;
+            this.TabPage_debug.Text = "调试";
+            // 
+            // Label_Console
+            // 
+            this.Label_Console.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.Label_Console.Location = new System.Drawing.Point(45, 14);
+            this.Label_Console.Name = "Label_Console";
+            this.Label_Console.Size = new System.Drawing.Size(100, 23);
+            this.Label_Console.TabIndex = 0;
+            this.Label_Console.Text = "显示控制台";
+            // 
+            // CheckBox_Console
+            // 
+            this.CheckBox_Console.Location = new System.Drawing.Point(14, 12);
+            this.CheckBox_Console.Name = "CheckBox_Console";
+            this.CheckBox_Console.Size = new System.Drawing.Size(25, 25);
+            this.CheckBox_Console.TabIndex = 1;
+            // 
+            // Button_Cancel
+            // 
+            this.Button_Cancel.Location = new System.Drawing.Point(257, 566);
+            this.Button_Cancel.Name = "Button_Cancel";
+            this.Button_Cancel.Size = new System.Drawing.Size(75, 23);
             this.Button_Cancel.TabIndex = 0;
             this.Button_Cancel.Text = "取消";
             this.Button_Cancel.Click += new System.EventHandler(this.Button_Cancel_Click);
-            this.Controls.Add(Button_Cancel);
-
-            this.Button_OK = new System.Windows.Forms.Button();
-            this.Button_OK.Size = new System.Drawing.Size(Button_Cancel.Width, Button_Cancel.Height);
-            this.Button_OK.Location = new System.Drawing.Point(Button_Cancel.Left - spacing * 6, Button_Cancel.Top);
-            this.Button_OK.Name = "确认";
+            // 
+            // Button_OK
+            // 
+            this.Button_OK.Location = new System.Drawing.Point(176, 566);
+            this.Button_OK.Name = "Button_OK";
+            this.Button_OK.Size = new System.Drawing.Size(75, 23);
+            this.Button_OK.TabIndex = 2;
             this.Button_OK.Text = "确认";
             this.Button_OK.Click += new System.EventHandler(this.Button_OK_Click);
-            this.Controls.Add(Button_OK);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(344, 601);
+            this.Controls.Add(this.TabControl1);
+            this.Controls.Add(this.Button_Cancel);
+            this.Controls.Add(this.Button_OK);
+            this.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(360, 640);
+            this.MinimumSize = new System.Drawing.Size(360, 640);
+            this.Name = "Form1";
+            this.Text = "Seewo Note Shot - 配置工具";
+            this.TabControl1.ResumeLayout(false);
+            this.TabPage_debug.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
@@ -144,5 +179,9 @@ namespace Seewo_Note_Shot___配置工具
 
         private Button Button_OK;
         private Button Button_Cancel;
+
+        private TabControl TabControl1;
+        private TabPage TabPage_save;
+        private TabPage TabPage_debug;
     }
 }
